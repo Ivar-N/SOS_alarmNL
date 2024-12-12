1acc733a4582da0305f488b11287e197bbdef593
Ivar-N <ihw.nouwens@student.avans.nl>
2024-12-12 11:43:48 +0100

    Create DeviceEventController.php

---
+++ SOS_Alarm/app/app/http/controllers/API/V1/DeviceEventController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\app\http\controllers\API\V1;
+
+class DeviceEventController
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
